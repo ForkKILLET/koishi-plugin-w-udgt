9103968333119931498b55dfebbe8f6af89e028d
ForkΨKILLET <fork_killet@outlook.com>
2024-08-11 02:29:13 +0800

    feat: >_<

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Context, Schema } from 'koishi'
+import { Context, Schema, h } from 'koishi'
 
 export const name = 'w-udgt'
 
@@ -7,5 +7,6 @@ export interface Config {}
 export const Config: Schema<Config> = Schema.object({})
 
 export function apply(ctx: Context) {
-  // write your plugin here
+    ctx.command(h.escape('_<'))
+        .action(() => '>_<')
 }
